c3db8d67611ac9d2f6ee0764da598fc48de9f3ab
Mack Miller <mack.matt.miller@gmail.com>
2018-04-06 22:40:17 -0500

    html.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFoundPage = () => (
   <div>
-    <h1>NOT FOUND</h1>
+    <h2>NOT FOUND</h2>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </div>
 )
