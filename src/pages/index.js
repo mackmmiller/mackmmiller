7e58fa06b977ce11f13bab385d5cefaff76f28eb
Mack Miller <mack.matt.miller@gmail.com>
2018-04-06 22:54:42 -0500

    landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,23 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   margin: auto;
   max-width: 960px;
-  > h1 {
+  > h2 {
     color: #e1483d;
     text-align: center;
     font-size: 6.25vw;
     margin: 0;
   }
+  > h3 {
+    font-weight: lighter;
+    margin: 0;
+    text-align: center;
+  }
 `
 
 const IndexPage = () => (
   <Wrapper>
-    <h1>I'm not sure what my tagline should be.</h1>
+    <h2>Fullstack Web Development.</h2>
+    <h3>Looking for freelance work, contracts, and full-time roles.</h3>
   </Wrapper>
 )
 
